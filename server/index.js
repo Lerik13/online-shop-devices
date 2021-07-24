88422c2cc45234f3e40e8b6d0aae6e5ad39ad23d
Valeria <lerik13@gmail.com>
2021-07-24 16:50:08 -0600

    deviceController

--- server/index.js
+++ server/index.js
@@ -3,14 +3,18 @@ const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
 const ErrorHandler = require('./midleware/ErrorHandlingMiddleware')
+const path = require('path')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
 app.use(express.json()) // чтобы приложение могло парсить JSON формат
+app.use(express.static(path.resolve(__dirname, 'static'))) // for access static files like imgs
+app.use(fileUpload({}))
 app.use('/api', router)
 
 // Обработка ошибок, последний Middleware
