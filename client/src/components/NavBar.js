96106b892e5a66c148e0c66f56d3b7dd8fbb02a3
Valeria <lerik13@gmail.com>
2021-08-02 00:09:34 -0600

    Admin page: Modals windows: CreateType, CreateBrand, CreateDevice

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,13 +2,15 @@ import React, { useContext } from 'react';
 import { Context } from '../index';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import { SHOP_ROUTE } from '../utils/consts';
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from '../utils/consts';
 import { NavLink } from 'react-router-dom';
 import {Button, Container} from 'react-bootstrap';
 import {observer} from 'mobx-react-lite';
+import {useHistory} from 'react-router-dom';
 
 const NavBar = observer(() => {
 	const {user} = useContext(Context)
+	const history = useHistory();
 
 	return (
 		<Navbar bg="dark" variant="dark">
@@ -16,8 +18,19 @@ const NavBar = observer(() => {
 			<NavLink style={{color: 'white'}} to={SHOP_ROUTE}>Buy device</NavLink>
 			{user.isAuth ?
 				<Nav style={{marginLeft: 'auto', color: 'white'}}>
-					<Button variant={"outline-light"}>Admin panel</Button>
-					<Button variant={"outline-light"} style={{marginLeft: '20px'}}>Enter</Button>
+					<Button 
+						variant={"outline-light"} 
+						onClick={() => history.push(ADMIN_ROUTE)}
+					>
+						Admin panel
+					</Button>
+					<Button 
+						variant={"outline-light"}
+						style={{marginLeft: '20px'}}
+						onClick={() => history.push(LOGIN_ROUTE)}
+					>
+						Log out
+					</Button>
 				</Nav>
 				:
 				<Nav style={{marginLeft: 'auto', color: 'white'}}>
