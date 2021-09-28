11f26b0f8589793bd70bc695eb80a6df0903c638
Valeria <lerik13@gmail.com>
2021-09-28 12:31:48 -0600

    load Brands and Devices from DB

--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -3,23 +3,8 @@ import {makeAutoObservable} from "mobx";
 export default class DeviceStore {
 	constructor() {
 		this._types = []
-		this._brands = [
-			{id: 1, name: 'Samsung'},
-			{id: 2, name: 'Apple'},
-			{id: 3, name: 'Lenovo'},
-			{id: 4, name: 'Asus'},
-			{id: 5, name: 'LG'},
-			{id: 6, name: 'Blackberry'},
-		]
-		this._devices = [
-			{id: 1, name: 'Iphone 12 pro', rating: 5, img: '', price: 1500},
-			{id: 2, name: 'Iphone 10 pro', rating: 5, img: '', price: 2500},
-			{id: 3, name: 'Iphone 11 pro', rating: 5, img: '', price: 3500},
-			{id: 4, name: 'Iphone X', rating: 5, img: '', price: 1500},
-			{id: 5, name: 'Iphone 10 pro', rating: 5, img: '', price: 15000},
-			{id: 6, name: 'Iphone 11 pro', rating: 5, img: '', price: 15000},
-			{id: 7, name: 'Iphone X', rating: 5, img: '', price: 15000},
-		]
+		this._brands = []
+		this._devices = []
 		this._selectedType = {}
 		this._selectedBrand = {}
 		makeAutoObservable(this)
