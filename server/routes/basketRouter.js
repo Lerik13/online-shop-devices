9129172bcd2be2ba9ea41f696bdc1594e47db4ae
Valeria <lerik13@gmail.com>
2023-02-01 17:04:15 -0700

    delete checkUserMiddleware

--- server/routes/basketRouter.js
+++ server/routes/basketRouter.js
@@ -2,7 +2,6 @@ const Router = require('express')
 const router = new Router()
 const basketController = require('../controllers/basketController')
 const authMiddleware = require('../middlewares/authMiddleware')
-//const checkUser = require('../middleware/checkUserMiddleware')
 
 router.post('/', authMiddleware, basketController.add)
 router.get('/', authMiddleware, basketController.getAll)
